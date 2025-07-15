8c406e746109f6b49d08c802dc7b6fd8185cb404
noam emilio radetich <noam81@yahoo.it>
2025-07-15 10:42:28 +0200

    working on the MAs

---
+++ technical_indicators/btc_moving_averages.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pandas_gbq
+from google.cloud import bigquery
+from typing import Any
+from google.oauth2 import service_account
+import logging
+
+destination_table = "btc_moving_averages"
+
+logger = logging.getLogger(__name__)
+
+def get_closing_prices(credentials) -> pd.DataFrame:
+   
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
+    query = """
+        SELECT 
+          timestamp,
+          price
+        FROM connection-123.signals.bitcoin_price
+        ORDER BY 1 DESC
+    """
+    query_job = client.query(query)
+    results = query_job.result()
+    bitcoin_prices = results.to_dataframe()
+
+    bytes_processed = query_job.total_bytes_processed
+    logger.info(f"Query processed {bytes_processed:,} bytes ({bytes_processed / 1024 / 1024:.2f} MB)")
+
+    return bitcoin_prices
